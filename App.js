58154ec37f3a67652cfefb131cd82379e626addb
Areeba Khan <kahnareeba@gmail.com>
2022-06-23 18:04:54 -0500

    Addds all rn files

---
+++ App.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import LoginScreen from './lib/LoginScreen';
+import Profile from './lib/Profile';
+import RegisterScreen from './lib/RegisterScreen';
+import Event from './lib/Event/Event';
+
+import RegistrationRequests from './lib/Admin/RegistrationRequests';
+import Directory from './lib/Admin/Directory';
+import Home from './lib/Home';
+import AlertForm from './lib/AlertForm'
+
+// const Stack = createNativeStackNavigator();
+const Stack = createStackNavigator();
+const navTheme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    background: 'transparent',
+  },
+};
+
+
+export default class NavExample extends Component {
+    render() {
+        return (
+            <NavigationContainer theme={navTheme}>
+              <Stack.Navigator initialRouteName="LoginScreen">
+                <Stack.Screen name="LoginScreen" component={LoginScreen}/>
+                <Stack.Screen name="AlertForm" component={AlertForm}/>
+                <Stack.Screen name="Home" component={Home}/>
+                <Stack.Screen name="Profile" component={Profile}/>
+                <Stack.Screen name="Event" component={Event}/>
+                <Stack.Screen name="RegisterScreen" component={RegisterScreen}/>
+                <Stack.Screen name="RegistrationRequests" component={RegistrationRequests}/>
+                <Stack.Screen name="Directory" component={Directory}/>
+              </Stack.Navigator>
+            </NavigationContainer>
+        );
+    }
+}
