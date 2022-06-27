7ce149264a986bd198031ca84c10c54ae87575de
Areeba Khan <kahnareeba@gmail.com>
2022-06-26 19:40:41 -0500

    Adds view relationships

--- pf-server/app.js
+++ pf-server/app.js
@@ -126,6 +126,22 @@ app.delete('/users/delete', (request, response) => {
 	       }));
 })
 
+/* RELATIONSHIPS */
+
+app.put('/relationship/get', (request, response) => {
+    let email = request.body.email;
+	console.log("Retrive all realtionships for email: " + email)
+    pool.query('SELECT * from relationships WHERE person1 = $1 OR person2 = $1', [email])
+	.then(res => {
+	    console.log('DB response: ' + JSON.stringify(res.rows));
+	    response.send(res.rows);
+	})
+	.catch(err =>
+	       setImmediate(() => {
+		   throw err;
+	       }));
+})
+
 
 /*
 * Registration requests post, get, delete
