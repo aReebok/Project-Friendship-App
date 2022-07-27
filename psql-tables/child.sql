03bd833941b310fbe5c71e66e909ed4002f2127c
Areeba Khan <kahnareeba@gmail.com>
2022-07-27 15:13:52 -0500

    Adds child, relationship, and notifications sql tables

---
+++ psql-tables/child.sql
@@ -0,0 +1,8 @@
+create table child (
+    cid serial primary key,
+    fname text,
+    lname text,
+    dob text,
+    school text,
+    gender text,
+);
---
+++ psql-tables/childrelationship.sql
@@ -0,0 +1,9 @@
+-- There are 2 relationship tables, I will delete the other one
+-- and primarily use this one. I like the structure of this better
+
+create table childrelationship (
+    rid serial primary key,
+    childID text,
+    personEmail text,
+    isParent boolean,
+);
