aa827ba54df4fc5b2c84c8b168495d4f6703df5b
Areeba Khan <kahnareeba@gmail.com>
2022-07-31 11:20:42 -0500

    Puts create event modal in another file

--- pf-server/routes/Events.js
+++ pf-server/routes/Events.js
@@ -1,22 +1,36 @@
 const express = require('express');
 const router = express.Router();
-const pool = require('.');
 
+const pool = require('./index');
 
 router.post('/', (request, response) => {
     let { author, participant, stat, title,
-		descrip, eventDate, eventCreated} = request.body;
+		descrip, eventDate, eventCreated, eventLocation} = request.body;
 
 	console.log(`Got request to add an event, 
 			will add ${title} created by ${author} to database`);
     pool.query(
-	'INSERT INTO events (author, participant, stat, title, descrip, eventDate, eventCreated) VALUES ($1, $2, $3, $4, $5, $6, $7)',
-	       [author, participant, stat, title, descrip, eventDate, eventCreated])
+	'INSERT INTO events (author, participant, stat, title, descrip, eventDate, eventCreated, eventLocation) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)',
+	       [author, participant, stat, title, descrip, eventDate, eventCreated, eventLocation])
 	.then(res => {
 	    console.log('DB response: ' + res.rows[0]);
 	    response.sendStatus(200)
 	})
 	.catch(err =>
+	       setImmediate(() => {
+		   throw err;
+	       }));
+})
+
+router.put('/', (request, response) => {
+    console.log(`Got request to get events of given email`);
+	let email = request.body.email;
+	console.log("Check for cid: " + cid)
+    pool.query('SELECT * FROM events where author = ($1)', [email])
+	.then(res => {
+	    console.log('DB response: ' + JSON.stringify(res.rows[0]));
+	    response.send(res.rows[0]);
+	}).catch(err =>
 	       setImmediate(() => {
 		   throw err;
 	       }));
--- psql-tables/events.sql
+++ psql-tables/events.sql
@@ -2,22 +2,28 @@
 -- pending, approved, expired/completed/removed
 create table events (
     eid serial primary key,
-    author text,
-    participant text,
+    mentor text,
+    cid text,
     stat text default 'pending',
     title text,
     descrip text,
     eventDate text,
-    eventCreated text
+    eventCreated text,
+    eventLocation text
 );
 
 -- INSERT INTO events (author, participant, stat, title, descrip, eventDate) VALUES;
 
-create table eventChangeRequests (
-    eid text, 
-    creator text,
-    reciever text,
-    status text default 'pending' -- pending, approved
+-- Added a column for eventLocation
+-- ALTER TABLE events  ADD COLUMN IF NOT EXISTS eventLocation text
+-- ALTER TABLE events RENAME COLUMN author TO mentor;
+-- ALTER TABLE events RENAME COLUMN participant TO cid;
+
+-- create table eventChangeRequests (
+--     eid text, 
+--     creator text,
+--     reciever text,
+--     status text default 'pending' -- pending, approved
     
-); 
+-- ); 
 
