f16f0437d8b91e30e8333b37457e7349549c18d5
Areeba Khan <kahnareeba@gmail.com>
2022-08-01 13:14:39 -0500

    Updates confirmed events in DB

--- pf-server/routes/Events.js
+++ pf-server/routes/Events.js
@@ -5,13 +5,13 @@ const pool = require('./index');
 
 router.post('/', (request, response) => {
     let { author, cid, title,
-		descrip, eventDate, eventCreated, eventLocation} = request.body;
+		descrip, stat, eventDate, eventCreated, eventLocation} = request.body;
 
 	console.log(`Got request to add an event, 
 			will add ${title} created by ${author} to database`);
     pool.query(
-	'INSERT INTO events (author, cid, title, descrip, eventDate, eventCreated, eventLocation) VALUES ($1, $2, $3, $4, $5, $6, $7)',
-	       [author, cid, title, descrip, eventDate, eventCreated, eventLocation])
+	'INSERT INTO events (author, cid, title, stat, descrip, eventDate, eventCreated, eventLocation) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)',
+	       [author, cid, title, stat, descrip, eventDate, eventCreated, eventLocation])
 	.then(res => {
 	    console.log('DB response: ' + res.rows[0]);
 	    response.sendStatus(200)
@@ -52,4 +52,18 @@ router.put('/pending', (request, response) => {
 	       }));
 })
 
+router.delete('/', (request, response) => {
+    let eid = request.body.eid;
+    console.log(`Got request to delete event under eid: ${eid}
+     from EVENTS table if exists`);
+    pool.query('DELETE FROM events WHERE eid = $1', [eid])
+	.then(res => {
+	    console.log('DB response: ' + res.rows[0]);
+	    response.sendStatus(200)
+	})
+	.catch(err =>
+	       setImmediate(() => {
+		   throw err;
+	       }));
+})
 module.exports = router;
