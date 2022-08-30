250f56f716ecab8adaa86723ce08185face1471e
Areeba Khan <kahnareeba@gmail.com>
2022-08-29 20:35:23 -0500

    Most updated PSQL tables

--- psql-tables/users.sql
+++ psql-tables/users.sql
@@ -10,7 +10,7 @@ create table users (
     pronouns text
 );
 -- example insert
-INSERT INTO users (email, fname, lname, role, phone) VALUES ('khan6@stolaf.edu', 'Areeba', 'Khan', 'mentor', '123-456-7890', 'She/her/hers');
+-- INSERT INTO users (email, fname, lname, role, phone) VALUES ('khan6@stolaf.edu', 'Areeba', 'Khan', 'mentor', '123-456-7890', 'She/her/hers');
 
 -- Adding pronouns for parents and users 
 -- ALTER TABLE users ADD COLUMN IF NOT EXISTS pronouns text;
@@ -24,7 +24,9 @@ create table registerRequests (
     fname text,
     lname text,
     role text,
-    phone text
+    phone text,
+    pronouns text,
+    daterequested text
 );
 
 -- create table approvedEmails (
@@ -32,21 +34,21 @@ create table registerRequests (
 --     role text
 -- );
 
-create table sessions (
-    sid text primary key, -- secure session key
-    email text            -- authenticated email 
-);
+-- create table sessions (
+--     sid text primary key, -- secure session key
+--     email text            -- authenticated email 
+-- );
 
-create function email(text) returns text as $$
-    select email from sessions where sid = $1;
-$$ language 'sql';
+-- create function email(text) returns text as $$
+--     select email from sessions where sid = $1;
+-- $$ language 'sql';
 
--- postgres function update_profile()
---    3 args type text: email, fname, lname, role, phone
---    no return
-create function approveUserRegistration(text, text, text, text, text) returns void as $$
-    DELETE FROM registerrequests WHERE email = $1;
-    INSERT INTO users VALUES ($1, $2, $3, $4, $5);
-$$ language 'sql';
+-- -- postgres function update_profile()
+-- --    3 args type text: email, fname, lname, role, phone
+-- --    no return
+-- create function approveUserRegistration(text, text, text, text, text) returns void as $$
+--     DELETE FROM registerrequests WHERE email = $1;
+--     INSERT INTO users VALUES ($1, $2, $3, $4, $5);
+-- $$ language 'sql';
 
 
